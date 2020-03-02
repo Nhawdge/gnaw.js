145cb4a50db3fe449fed1a618680f965e2aa6d87
John Pavek <JohnQPavek@gmail.com>
2020-03-01 22:14:38 -0600

    Adds some basic stuff

---
+++ src/ts/index.ts
@@ -0,0 +1,19 @@
+console.log("Start app")
+
+var todo = `
+1. Create /docs/ folder
+2. steps to make
+3. Header file
+4. footer file.
+5. template
+6. web resources
+7. compile and push?
+8. Update docs
+`
+
+console.log(todo)
+
+
+function main() {
+
+}
