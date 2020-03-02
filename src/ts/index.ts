2b8c9a737f44707e5e5c03e3c2b02d9049f6c109
John Pavek <JohnQPavek@gmail.com>
2020-03-01 23:32:24 -0600

    Adding more ideas

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -9,11 +9,21 @@ var todo = `
 6. web resources
 7. compile and push?
 8. Update docs
+
 `
 
 console.log(todo)
 
 
 function main() {
+var startupSteps =`
+Create a config
+Create a home page
+Home page is pure on page editing
+Nav auto generates by route
+Add options to create a reuseable block
+Save to template folder
+Build from template folder
+`
 
 }
